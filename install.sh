bb2c93c7daa3f2b5e8ede142c650f9d16956f71b
Bartosz Matera <thebartti@gmail.com>
2018-02-28 03:48:07 +0100

    Added new prompts, removed unusued stuff from install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DOTFILES_CACHE="$DOTFILES_DIR/.cache.sh"
 
 # Make utilities available
-PATH="$DOTFILES_DIR/bin:$PATH"
+#PATH="$DOTFILES_DIR/bin:$PATH"
 
 # Update dotfiles itself first
-if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
+#if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
 
 # Bunch of symlinks
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
